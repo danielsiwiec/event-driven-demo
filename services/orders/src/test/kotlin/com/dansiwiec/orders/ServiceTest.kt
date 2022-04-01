db8443e4382f00b943d63a76ab905ab37736cafb
Dan Siwiec <daniel.siwiec@gmail.com>
2022-03-31 21:03:10 -0700

    Simplify test status code comparison

--- services/orders/src/test/kotlin/com/dansiwiec/orders/ServiceTest.kt
+++ services/orders/src/test/kotlin/com/dansiwiec/orders/ServiceTest.kt
@@ -5,7 +5,6 @@ import com.dansiwiec.orders.models.Order
 import com.dansiwiec.orders.models.OrderRequest
 import com.dansiwiec.orders.models.Sku
 import com.dansiwiec.orders.repository.SkusRepository
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.awaitility.Awaitility.await
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
@@ -15,6 +14,7 @@ import org.junit.jupiter.api.fail
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.utils.KafkaTestUtils
 import java.time.Duration
@@ -48,7 +48,7 @@ class ServiceTest : KafkaTestBase() {
         )
         val orderId = response.body!!.id
 
-        assertThat(response.statusCode.value(), equalTo(200))
+        assertThat(response.statusCode, equalTo(HttpStatus.OK))
         val singleRecord = KafkaTestUtils.getSingleRecord(consumer, Topics.ORDERS)
         assertThat(singleRecord.key(), equalTo(orderId.toString()))
 
@@ -69,7 +69,7 @@ class ServiceTest : KafkaTestBase() {
         val response = testRestTemplate.postForEntity(
             "/orders", OrderRequest(items = listOf(LineItem("1", 1), LineItem("3", 2))), Order::class.java
         )
-        assertThat(response.statusCode.value(), equalTo(400))
+        assertThat(response.statusCode, equalTo(HttpStatus.BAD_REQUEST))
         val records = KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(5).toMillis())
         assertThat(records.count(), equalTo(0))
     }
@@ -82,7 +82,7 @@ class ServiceTest : KafkaTestBase() {
         )
         val records = KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(5).toMillis())
 
-        assertThat(response.statusCode.value(), equalTo(400))
+        assertThat(response.statusCode, equalTo(HttpStatus.BAD_REQUEST))
         assertThat(records.count(), equalTo(0))
     }
 

ed90cf5bb529dbabc28252192878007b940c80ab
Dan Siwiec <daniel.siwiec@gmail.com>
2022-10-22 21:16:00 -0700

    extract scenarios into functions

--- tests/performance/src/gatling/kotlin/BasicSimulation.kt
+++ tests/performance/src/gatling/kotlin/BasicSimulation.kt
@@ -4,42 +4,41 @@ import io.gatling.javaapi.http.HttpDsl.http
 import io.gatling.javaapi.http.HttpDsl.status
 import java.time.Duration
 
-class BasicSimulation : Simulation() {
+    class BasicSimulation : Simulation() {
+        init {
+            setUp(
+                scenario("order happy path")
+                    .group("Process Order").on(
+                        exec(resetCounter())
+                            .exec(postOrder())
+                            .exec(waitForPaymentToProcess())
+                    ).injectClosed(constantConcurrentUsers(1).during(Duration.ofSeconds(30)))
+            ).protocols(
+                http
+                    .contentTypeHeader("application/json")
+                    .acceptHeader("application/json")
+            )
+        }
+    }
 
-    init {
-
-        val httpProtocol = http.contentTypeHeader("application/json").acceptHeader("application/json")
-
-        val postOrder = http("create order")
-            .post("http://localhost:8080/orders").body(
-                StringBody(
-                    """
+fun postOrder() = http("create order")
+    .post("http://localhost:8080/orders").body(
+        StringBody(
+            """
                     {
                         "items": [{"sku":"1", "quantity": 1}],
                         "customerId": "1"
                      }
                 """.trimIndent()
-                )
-            ).check(status().`is`(200))
-
-        val resetPaymentCounter = http("reset payment counter").post("http://localhost:8081/sentPaymentsCount/reset")
-
-        val waitForPaymentToProcess = doWhile { !it.getString("count").equals("1") }.on(
-            exec(
-                http("get payment counter")
-                    .get("http://localhost:8081/sentPaymentsCount")
-                    .check(bodyString().saveAs("count"))
-            )
         )
+    ).check(status().`is`(200))
 
-        setUp(
-            scenario("order happy path")
-                .group("Process Order").on(
-                    exec(resetPaymentCounter)
-                        .exec(postOrder)
-                        .exec(waitForPaymentToProcess)
-                )
-                .injectClosed(constantConcurrentUsers(1).during(Duration.ofSeconds(30)))
-        ).protocols(httpProtocol)
-    }
-}
+fun resetCounter() = http("reset payment counter").post("http://localhost:8081/sentPaymentsCount/reset")
+
+fun waitForPaymentToProcess() = doWhile { !it.getString("count").equals("1") }.on(
+    exec(
+        http("get payment counter")
+            .get("http://localhost:8081/sentPaymentsCount")
+            .check(bodyString().saveAs("count"))
+    )
+)
